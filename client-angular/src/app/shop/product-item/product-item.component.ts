9ab8284cff906e76a9f377f7f3d482e17ca93bd2
cx20012002 <cx20012002@gmail.com>
2022-10-04 18:58:47 +1300

    End of section 14

--- client-angular/src/app/shop/product-item/product-item.component.ts
+++ client-angular/src/app/shop/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IProduct} from "../../shared/models/product";
+import {BasketService} from "../../basket/basket.service";
 
 @Component({
   selector: 'app-product-item',
@@ -9,10 +10,14 @@ import {IProduct} from "../../shared/models/product";
 export class ProductItemComponent implements OnInit {
   @Input() product: IProduct;
 
-  constructor() {
+  constructor(private basketService: BasketService) {
   }
 
   ngOnInit(): void {
+  }
+
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product);
   }
 
 }
