31128c5c6c7cb4414f2cbb4aad3239688a28fe79
cx20012002 <cx20012002@gmail.com>
2023-04-15 22:15:58 +1200

    Section 1

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
